12f417e63ff423d627835f43ff533bc56bb7d956
Ba7Le <huynhcamhan1235@gmail.com>
2023-05-28 19:45:33 +0700

    add ngx-ui-loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgxUiLoaderModule, NgxUiLoaderConfig, SPINNER, PB_DIRECTION } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  text: 'Loading...',
+  textColor: '#FFFFFF',
+  textPosition: 'center-center',
+  pbColor: 'red',
+  bgsColor: 'red',
+  fgsColor: 'red',
+  fgsType: SPINNER.cubeGrid,
+  fgsSize: 100,
+  pbDirection: PB_DIRECTION.leftToRight,
+  pbThickness: 5
+}
 
 // Function to load translations from a JSON file
 export function HttpLoaderFactory(http: HttpClient) {
@@ -25,6 +39,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     CoreModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
     TranslateModule.forRoot({
       defaultLanguage: 'en', // Set the default language
       loader: {
