98505b1665bbd023f1c82fb922ec933b76600c3c
Ba7Le <huynhcamhan1235@gmail.com>
2023-05-28 15:50:38 +0700

    routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './core/component/layout/layout.component';
+import { PageNotFoundComponent } from './core/component/statis-page/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,13 +6,15 @@ import { LayoutComponent } from './component/layout/layout.component';
 import { HeaderComponent } from './component/layout/component/header/header.component';
 import { BreadcrumbComponent } from './component/layout/component/breadcrumb/breadcrumb.component';
 import { SharedModule } from '../shared/shared.module';
+import { PageNotFoundComponent } from './component/statis-page/page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
     HeaderComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    PageNotFoundComponent
   ],
   imports: [
     CommonModule,
